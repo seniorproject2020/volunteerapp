3f775018fef5904082ae445e18e14e49b87acb80
Isaiah Bonds <isaiah.bonds@outlook.com>
2020-02-01 16:21:11 -0500

    Need to merge schema changes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,27 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
 import apiRouter from './routes';
+require('dotenv').config();
+
 
 const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+const db: string = process.env.MONGO_URI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 app.use(express.static('public'));
 app.use(apiRouter);
